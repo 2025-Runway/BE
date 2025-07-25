db7a4526d0952f1e250f16e9901ab133f5aacc64
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-07-25 23:39:53 +0900

    initial commit

---
+++ src/main/java/com/example/runway/RunwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.runway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunwayApplication.class, args);
+	}
+
+}
