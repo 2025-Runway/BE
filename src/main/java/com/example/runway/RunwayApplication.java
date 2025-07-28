1d84df5d85b4c4d37b51996624a5f4476fd2c1c4
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-07-28 21:57:38 +0900

    chore: baseEntity 설정

--- src/main/java/com/example/runway/RunwayApplication.java
+++ src/main/java/com/example/runway/RunwayApplication.java
@@ -2,8 +2,10 @@ package com.example.runway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RunwayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RunwayApplication.class, args);
