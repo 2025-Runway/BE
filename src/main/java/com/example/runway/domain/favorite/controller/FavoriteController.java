5271682a5eba52fbb6bae94d929b8e2e1a9aaa51
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-09-14 11:15:35 +0900

    fix: swagger 명세 추가

--- src/main/java/com/example/runway/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/example/runway/domain/favorite/controller/FavoriteController.java
@@ -3,6 +3,7 @@ package com.example.runway.domain.favorite.controller;
 
 import com.example.runway.domain.favorite.service.FavoriteService;
 import com.example.runway.global.jwt.annotation.LoginUserId;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,10 @@ public class FavoriteController {
     private final FavoriteService favoriteService;
 
     @PostMapping("/{crsIdx}/favorite")
+    @Operation(
+            summary = "코스 추천 API",
+            description = "코스 추천해주는 API"
+    )
     public void favorite(@LoginUserId Long userId,
                          @PathVariable("crsIdx") String crsIdx) {
 
