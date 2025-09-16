1e9201a9f999a3c8ea10e702ed2e946ec8e6c642
김민석 <skyfox001015@naver.com>
2025-09-16 17:06:02 +0900

    feat : swagger 설명 작성
    Weather, Auth Controller 설명 추가

--- src/main/java/com/example/runway/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/runway/domain/auth/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.example.runway.domain.auth.dto.LoginResponse;
 import com.example.runway.domain.auth.dto.TestLoginRequest;
 import com.example.runway.domain.auth.dto.TestLoginResponse;
 import com.example.runway.domain.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,8 +22,13 @@ public class AuthController {
 
 
     @GetMapping("/auth/login/kakao")
-    public ResponseEntity<?> kakaoLogin(@RequestParam("code") String accessCode) {
-
+    @Operation(
+            summary = "카카오 로그인 API",
+            description = "카카오 로그인. 프론트에서 인증코드 받아서 전달. redirectUrl : https://api.run-way.site/auth/login/kakao"
+    )
+    public ResponseEntity<?> kakaoLogin(
+            @Parameter(description = "인가 코드", example = "aBcDeFgHiJkLmNoPqRsTuVwXyZ12345")
+            @RequestParam("code") String accessCode) {
         LoginResponse result = authService.oAuthLogin(accessCode);
 
         log.info("kakao login result: {}", result);
@@ -32,7 +39,13 @@ public class AuthController {
     }
 
     @PostMapping("/auth/test")
-    public ResponseEntity<TestLoginResponse> testLogin(@RequestBody TestLoginRequest request) {
+    @Operation(
+            summary = "테스트 로그인 API",
+            description = "테스트 로그인 emil로만 로그인"
+    )
+    public ResponseEntity<TestLoginResponse> testLogin(
+            @Parameter(description = "유저 이메일", example = "test@run-way.site")
+            @RequestBody TestLoginRequest request) {
         TestLoginResponse response = authService.testLogin(request.email());
         return ResponseEntity.ok(response);
     }
