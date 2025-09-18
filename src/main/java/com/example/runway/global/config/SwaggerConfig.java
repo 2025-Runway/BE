233a4ca9443b63a280194b1bfd5cbba85b23ee92
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-09-18 22:36:05 +0900

    hotfix: 변수명 수정

--- src/main/java/com/example/runway/global/config/SwaggerConfig.java
+++ src/main/java/com/example/runway/global/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
-    @Value("${server.url}")
+    @Value("${server.server-url}")
     private String SERVER_URL;
 
     @Bean
